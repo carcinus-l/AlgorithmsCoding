c63a7cb83839c6a29bfbab0a2a880ad4401b927e
q30040315 <qinliaoming@huawei.com>
2023-02-13 16:52:54 +0800

    update

---
+++ src/main/java/com/carcinus/code/hw/Topic1.java
@@ -0,0 +1,22 @@
+package com.carcinus.code.hw;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ 任务混部
+ 因为没有限制结束时间，所以只需要取最大并行度
+
+ */
+public class Topic1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = null;
+        while ((line = br.readLine()) != null) {
+
+        }
+    }
+
+}
