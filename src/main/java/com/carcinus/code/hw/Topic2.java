b87465a9712588b024b3b88f3dcdfe947fbc403c
q30040315 <qinliaoming@huawei.com>
2023-02-14 10:13:59 +0800

    update

--- src/main/java/com/carcinus/code/hw/Topic2.java
+++ src/main/java/com/carcinus/code/hw/Topic2.java
@@ -1,5 +1,9 @@
 package com.carcinus.code.hw;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
 
  单词倒序
@@ -8,7 +12,36 @@ package com.carcinus.code.hw;
 
  */
 public class Topic2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            String reverseWord = reverseWord(line);
+            System.out.println(reverseWord);
+        }
+    }
+
+    public static String reverseWord(String s) {
+        StringBuffer sb = new StringBuffer();
+        String[] words = s.split(" ");
+        for (String word : words) {
+            sb.append(" ");
+            char[] chars = word.toCharArray();
+            int index = chars.length - 1;
+            char op = ' ';
+            if (chars[index] == ',' || chars[index] == '.' || chars[index] == '?') {
+                op = chars[index];
+                index --;
+            }
+            while (index >= 0) {
+                sb.append(chars[index--]);
+            }
+            if (op != ' ') {
+                sb.append(op);
+            }
+        }
+        return sb.substring(1);
 
     }
+
 }
