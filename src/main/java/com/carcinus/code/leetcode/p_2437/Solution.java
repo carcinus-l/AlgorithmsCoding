bbca8aac4d05940fd950a858491bd9e08a73a7ea
q30040315 <qinliaoming@huawei.com>
2023-02-20 10:55:33 +0800

    update

---
+++ src/main/java/com/carcinus/code/leetcode/p_2437/Solution.java
@@ -0,0 +1,34 @@
+package com.carcinus.code.leetcode.p_2437;
+
+/**
+ * @link https://leetcode.cn/problems/best-poker-hand/
+ */
+public class Solution {
+
+    public String bestHand(int[] ranks, char[] suits) {
+        int[] rc = new int[14];
+        int[] sc = new int[4];
+        for (int i = 0; i < 5; i++) {
+            //相同的牌计数
+            rc[ranks[i]]++;
+            //花色计数
+            sc[(suits[i] - 'a')]++;
+        }
+        for (int count : sc) {
+            if (count == 5) {
+                return "Flush";
+            }
+        }
+        int max = 0;
+        for (int count : rc) {
+            max = Math.max(count, max);
+        }
+        if (max >= 3) {
+            return "Three of a Kind";
+        } else if (max == 2) {
+            return "Pair";
+        } else {
+            return "High Card";
+        }
+    }
+}
