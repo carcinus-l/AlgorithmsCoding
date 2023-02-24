9c3cf4b54a83aa4b50be9d51f23ea36ce4d56206
q30040315 <qinliaoming@huawei.com>
2023-02-24 09:25:41 +0800

    update

---
+++ src/main/java/com/carcinus/code/leetcode/p_2357/MakeArrayZeroBySubtractingEqualAmounts.java
@@ -0,0 +1,19 @@
+package com.carcinus.code.leetcode.p_2357;
+
+/**
+ * 2357. 使数组中所有元素都等于零
+ */
+public class MakeArrayZeroBySubtractingEqualAmounts {
+
+    public int minimumOperations(int[] nums) {
+        int[] flags = new int[101];
+        int count = 0;
+        for (int num : nums) {
+            if (num != 0 && flags[num] == 0) {
+                flags[num]++;
+                count++;
+            }
+        }
+        return count;
+    }
+}
